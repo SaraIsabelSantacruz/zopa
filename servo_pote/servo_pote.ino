26085a64af69913e3c1d695963c91551f19b3039
SaraIsabelSantacruz <sara.santacruz@wolox.co>
2023-06-10 17:10:38 -0300

    servo-pote

---
+++ servo_pote/servo_pote.ino
@@ -0,0 +1,27 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+#define SERVOMIN  80 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  505 // This is the 'maximum' pulse length count (out of 4096)
+#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+uint8_t servonum = 0;
+
+float valorPote;
+float valorAnterior;
+float pulso;
+
+void setup() {
+  Serial.begin(9600);
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(SERVO_FREQ);
+}
+
+void loop() {
+  valorPote = analogRead(A0);
+  int grados = map(valorPote, 0, 1023, 0, 360);
+  int pulso = map(grados, 0, 360, 80, 490);
+  pwm.setPWM(0, 0, pulso);
+}
